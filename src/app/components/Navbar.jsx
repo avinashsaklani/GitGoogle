3b72ac1a707fba84f9b55ad9e14175f623b73ede
avinashsaklani <avinashsaklani7@gmail.com>
2023-06-29 15:19:48 +0530

    final commit

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,25 +1,24 @@
-import { Button,  Box, Flex, useDisclosure } from '@/app/chakra'
-import { Image } from '@/app/chakra-next'
-import React from 'react'
-import HistoryModal from './HistoryModal';
-
+import { Button, Box, Flex, useDisclosure } from "@/app/chakra";
+import { Image } from "@/app/chakra-next";
+import React from "react";
+import HistoryModal from "./HistoryModal";
+import LOGO from "../../../public/logo.png";
 
 const Navbar = () => {
-   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Flex justifyContent={"space-between"} py={6} alignItems={"center"}>
-    <Box position={"relative"} aspectRatio={5/3} minHeight={20}>
-<Image src={"/logo.png"}   fill alt='github_logo' sx={{filter:"invert(1)"}}/>
-    </Box>
-    <Box>
-        <Button size="md"  colorScheme={'whatsapp'} onClick={onOpen}>
-Search History
+      <Box position={"relative"} aspectRatio={5 / 3} minHeight={20}>
+        <Image src={LOGO} fill alt="github_logo" sx={{ filter: "invert(1)" }} />
+      </Box>
+      <Box>
+        <Button size="md" colorScheme={"whatsapp"} onClick={onOpen}>
+          Search History
         </Button>
-
-    </Box>
-    {isOpen && <HistoryModal isOpen={isOpen} onClose={onClose}/>}
+      </Box>
+      {isOpen && <HistoryModal isOpen={isOpen} onClose={onClose} />}
     </Flex>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -1,77 +1,83 @@
-"use client"
-import { Input } from "@/app/chakra"
-import { Button, ButtonGroup, useToast } from "@/app/chakra"
-import { useState } from "react"
-const Search = ({setUserData, setLoading}) => {
-    const[query, setQuery] = useState('');
-    const toast = useToast()
+"use client";
+import { Input } from "@/app/chakra";
+import { Button, ButtonGroup, useToast } from "@/app/chakra";
+import { useState } from "react";
+const Search = ({ setUserData, setLoading }) => {
+  const [query, setQuery] = useState("");
+  const toast = useToast();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        // alert(`You searched for ${query}`);
-        if(!query) return;
-        setLoading(true);
-        setUserData(null);
-        try{
-            const res = await fetch(`https://api.github.com/users/${query}`);
-            const data = await res.json();
-            // console.log(data, "data is here")
-            if(data.message){
-                return toast({
-                    title: "Oops! It's an Error",
-                    description: data.message === "Not Found" ? "User not found" : data.message,
-                    status: "error",
-                    duration: 4000,
-                    isClosable: true,
-                });
-            }
-            setUserData(data);
-            addUserToLocalStorage(data,query);
-        }
-        catch(error){
-            toast({
-                    title: "Oops! It's an Error",
-                    description: error.message,
-                    status: "error",
-                    duration: 4000,
-                    isClosable: true
-                });
-
-        }finally{
-            setLoading(false);
-        }
-
-    };
-
-    const addUserToLocalStorage = (data, username) =>{
-        const users = JSON.parse(localStorage.getItem('github-users')) || [];
-        const userExists = users.find(user => user.id === username);
-        if(userExists){
-            users.splice(users.indexof(userExists),1);
-        }
-        users.unshift({
-            id:username,
-            avatar_url: data.avatar_url,
-            name: data.name,
-            url: data.html_url
-        })
-        localStorage.setItem('github-users', JSON.stringify(users));
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // alert(`You searched for ${query}`);
+    if (!query) return;
+    setLoading(true);
+    setUserData(null);
+    try {
+      const res = await fetch(`https://api.github.com/users/${query}`);
+      const data = await res.json();
+      // console.log(data, "data is here")
+      if (data.message) {
+        return toast({
+          title: "Oops! It's an Error",
+          description:
+            data.message === "Not Found" ? "User not found" : data.message,
+          status: "error",
+          duration: 4000,
+          isClosable: true,
+        });
+      }
+      setUserData(data);
+      addUserToLocalStorage(data, query);
+    } catch (error) {
+      toast({
+        title: "Oops! It's an Error",
+        description: error.message,
+        status: "error",
+        duration: 4000,
+        isClosable: true,
+      });
+    } finally {
+      setLoading(false);
     }
+  };
 
+  const addUserToLocalStorage = (data, username) => {
+    const users = JSON.parse(localStorage.getItem("github-users")) || [];
+    const userExists = users.find((user) => user.id === username);
+    if (userExists) {
+      users.splice(users.indexOf(userExists), 1);
+    }
+    users.unshift({
+      id: username,
+      avatar_url: data.avatar_url,
+      name: data.name,
+      url: data.html_url,
+    });
+    localStorage.setItem("github-users", JSON.stringify(users));
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-        <Input
+      <Input
         variant={"outline"}
         placeholder={"Type a username (i.e elonmusk)"}
         borderWidth={2}
         borderRadius={10}
         focusBorderColor="green.500"
         onChange={(e) => setQuery(e.target.value)}
-        />
-        <Button size="md" type="submit" colorScheme="whatsapp" mt={4} disabled={!query} opacity={!query ? 0.5 : 1}>Search</Button>
+      />
+      <Button
+        size="md"
+        type="submit"
+        colorScheme="whatsapp"
+        mt={4}
+        disabled={!query}
+        opacity={!query ? 0.5 : 1}
+      >
+        Search
+      </Button>
     </form>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
